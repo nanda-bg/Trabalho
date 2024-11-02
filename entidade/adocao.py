20f519b390365233900e657c24ba4e15e9c78264
Fernanda Barros <fernanda.girade@liven.tech>
2024-11-02 10:09:15 -0300

    ajuste para exibir relatorios corretamente

--- entidade/adocao.py
+++ entidade/adocao.py
@@ -3,30 +3,30 @@ from datetime import date
 
 class Adocao:
     def __init__(self, animal, adotante, termo_assinado = False, data = date.today().isoformat()):
-        self._animal = animal
-        self._data = data
-        self._adotante = adotante
-        self._termo_assinado = termo_assinado
+        self.__animal = animal
+        self.__data = data
+        self.__adotante = adotante
+        self.__termo_assinado = termo_assinado
 
     @property
     def ong(self):
-        return self._ong
+        return self.__ong
     
     @property
     def data(self):
-        return self._data
+        return self.__data
 
     @property
     def animal(self):
-        return self._animal
+        return self.__animal
 
     @property
     def adotante(self):
-        return self._adotante
+        return self.__adotante
 
     @property
     def termo_assinado(self):
-        return self._termo_assinado
+        return self.__termo_assinado
     
     @termo_assinado.setter
     def termo_assinado(self, termo_assinado):
@@ -34,7 +34,7 @@ class Adocao:
             print("termo_assinado deve ser um booleano")
             return
         
-        self._termo_assinado = termo_assinado
+        self.__termo_assinado = termo_assinado
 
     def __str__(self):
         return f"Adoção do animal {self.animal.nome} realizada por {self.adotante.nome} em {self.data}"        
--- entidade/pessoa.py
+++ entidade/pessoa.py
@@ -2,37 +2,37 @@ from abc import ABC
 
 class Pessoa(ABC):
     def __init__(self, cpf, nome, data_nascimento, endereco): 
-        self._nome = nome
+        self.__nome = nome
 
-        self._data_nascimento = data_nascimento
+        self.__data_nascimento = data_nascimento
 
-        self._endereco = endereco
+        self.__endereco = endereco
 
-        self._cpf = cpf
+        self.__cpf = cpf
 
     @property
     def cpf(self):
-        return self._cpf
+        return self.__cpf
     
     @property
     def nome(self):
-        return self._nome
+        return self.__nome
 
     @nome.setter
     def nome(self, nome):
-        self._nome = nome
+        self.__nome = nome
 
     @property
     def data_nascimento(self):
-        return self._data_nascimento
+        return self.__data_nascimento
 
     @property
     def endereco(self):
-        return self._endereco
+        return self.__endereco
 
     @endereco.setter
     def endereco(self, endereco):
-        self._endereco = endereco
+        self.__endereco = endereco
     
     def __str__(self):
         return f'Nome: {self.nome}, CPF: {self.cpf}, Data de nascimento: {self.data_nascimento}, Endereço: {self.endereco}'
